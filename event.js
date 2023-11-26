67ef531b055c50b0eba1b5bc185b3768c7ae97c9
Ulad L <ulad@Ulads-Air-M2.lan>
2023-11-26 11:14:24 -0600

    day1

---
+++ event.js
@@ -0,0 +1,11 @@
+window.onload = change
+
+function change (){
+   let picture = document.getElementById("photo")
+   picture.onclick = double
+}
+
+function double(){
+    let picture = document.getElementById("photo")
+     photo.src = "image/Visiable.jpg"
+}
