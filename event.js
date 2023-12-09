95f07b2c5fbd4fbbd32c8a90d69e0a1dfc4f453f
Ulad L <ulad@Ulads-Air-M2.lan>
2023-12-09 10:39:52 -0600

    Now you need to hover

--- event.js
+++ event.js
@@ -2,8 +2,8 @@ window.onload = init;
 
 function init(){
   var images = document. getElementsByTagName ("img") ;
-  for (var i = 0; i < images. length; i++)
-  images[i].onclick = showAnswer
+  for (var i = 0; i < images.length; i++)
+  images[i].onmousemove = showAnswer
   }
 function showAnswer (eventObj) {
   var image = eventObj.target;
